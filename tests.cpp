fe9d0b6e4de2fef80456958be728ca5c335c4cfa
TyRoXx <tyroxxxx@gmail.com>
2021-08-02 17:19:07 +0200

    test more cases

--- tests.cpp
+++ tests.cpp
@@ -34,6 +34,14 @@ TEST_CASE("snow falls")
     REQUIRE(expected == result);
 }
 
+TEST_CASE("snow falls with gaps")
+{
+    std::vector<Cell> world = { Cell::Snow, Cell::Snow, Cell::Air };
+    const std::vector<Cell> result = simulateStep(world.front(), sf::Vector2u(1, 3));
+    const std::vector<Cell> expected = { Cell::Snow, Cell::Air, Cell::Snow };
+    REQUIRE(expected == result);
+}
+
 TEST_CASE("snow collects at the bottom")
 {
     std::vector<Cell> world = { Cell::Snow };
@@ -47,5 +55,13 @@ TEST_CASE("wall stops snow")
     std::vector<Cell> world = { Cell::Snow, Cell::Wall };
     const std::vector<Cell> result = simulateStep(world.front(), sf::Vector2u(1, 2));
     const std::vector<Cell> expected = { Cell::Snow, Cell::Wall };
+    REQUIRE(expected == result);
+}
+
+TEST_CASE("snow stops snow")
+{
+    std::vector<Cell> world = { Cell::Snow, Cell::Snow };
+    const std::vector<Cell> result = simulateStep(world.front(), sf::Vector2u(1, 2));
+    const std::vector<Cell> expected = { Cell::Snow, Cell::Snow };
     REQUIRE(expected == result);
 }
