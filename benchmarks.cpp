c19b2bd3039e4e964d3750a5dad76a3a3b92caf1
TyRoXx <tyroxxxx@gmail.com>
2021-08-06 23:50:53 +0200

    compiles again

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -6,7 +6,7 @@ static void BM_simulateStep(benchmark::State& state)
     const Point worldSize(500, 500);
     World input(worldSize, Cell::Snow);
     for (auto _ : state) {
-        benchmark::DoNotOptimize(simulateStep(input.Cells.front(), worldSize));
+        benchmark::DoNotOptimize(simulateStep(input));
     }
 }
 BENCHMARK(BM_simulateStep)->Unit(benchmark::kMillisecond);
